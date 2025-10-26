f20ae9b94a5d3369912ab888375cdd30de2647ad
GroovinFlow <azuolasbalbieris@gmail.com>
2025-10-26 17:26:10 +0300

    Initial commit for Polymarket Hashdive analysis project

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+"""
+Polymarket Gamma API
+https://gamma-api.polymarket.com
+
+Hashdive API
+https://hashdive.com/api/
+
+pip install -r requirements.txt
+
+environment variables in .env file
+"""
+
